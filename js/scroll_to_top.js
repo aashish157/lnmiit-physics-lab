a3b8de1da90121e0ff568072d99f0096a25d5a4b
Aashish Raj Gupta <argupta157@gmail.com>
2018-05-29 00:20:14 +0530

    level3,videos updated

--- js/scroll_to_top.js
+++ js/scroll_to_top.js
@@ -1,8 +1,8 @@
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 40px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("scroll_btn").style.display = "block";
     } else {
         document.getElementById("scroll_btn").style.display = "none";
